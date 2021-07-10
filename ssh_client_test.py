eb9e9e046fcb4e85987c4a5ff7901a10ede3b268
Yao-Yu Li <yaoyuli1993@gmail.com>
2021-07-11 00:56:56 +0800

    Follow up updates of PTT and support output mode

--- ssh_client_test.py
+++ ssh_client_test.py
@@ -1,92 +1,70 @@
 #!/usr/bin/env python3
 
-
 import time
 import base64
 import paramiko
 
+_output_mode = False
 
 _host = "ptt.cc"
 _user = "bbsu"
 _pass = ""
+_key_file = "Your SSH private key path"
 
 _ptt_username = "Your Ptt username"
 _ptt_passwd = "Your Ptt password"
 
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname = _host, username = _user, password = _pass)
 
-chan = client.invoke_shell()
+def interact(chan, content):
+    for c in content:
+        if c is not None:
+            chan.send(c)
+            time.sleep(3)
+        else:
+            resp = chan.recv(9999)
+            time.sleep(3)
 
-# Ptt welcome page
-chan.recv(9999).decode('big5')
-time.sleep(3)
+        if _output_mode and c is None:
+            try:
+                print(resp.decode('utf-8'))
+            except UnicodeDecodeError:
+                print(resp.decode('big5'))
 
-# Enter username
-chan.send(_ptt_username + "\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
 
-# Enter password
-chan.send(_ptt_passwd + "\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
+def main():
+    client = paramiko.SSHClient()
+    client.load_host_keys(_key_file)
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname = _host, username = _user, password = _pass)
 
-# Input enter for passing bridge page
-chan.send("\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
+    chan = client.invoke_shell()
 
-# Show the board page
-chan.recv(9999).decode()
-time.sleep(3)
+    # Ptt welcome page
+    interact(chan, [None])
 
-# Enter test board
-chan.send("s")
-chan.send("test\r\n")
-time.sleep(3)
-chan.recv(9999).decode()
-chan.send("\r\n")
-chan.recv(9999).decode()
+    # Enter username
+    interact(chan, [_ptt_username + '\r\n', None])
 
-# Post test article in test board
-chan.send("\x10") # Enter post page
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("1\r\n") # Article type
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("Test\r\n") # Title
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("Test") # Content
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("\x18")
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("s\r\n") # Save article
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("\r\n") # Save article
-chan.recv(9999).decode()
-time.sleep(3)
+    # Enter password
+    interact(chan, [_ptt_passwd + '\r\n', None])
 
-# Leave Ptt
-chan.send("\x1b[D")
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("\x1b[D")
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("y\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
-chan.send("\r\n")
-chan.recv(9999).decode()
-time.sleep(3)
+    # Input enter for passing bridge page
+    interact(chan, ['\r\n', None])
+
+    # Enter test board
+    interact(chan, ['s', 'test\r\n', None, '\r\n', None])
+
+    # Post test article in test board
+    interact(chan, ['\x10', None])  # Enter post page
+    interact(chan, ['1\r\n', None])  # Article type
+    interact(chan, ['Test\r\n', None])  # Title
+    interact(chan, ['Test\r\n', None])  # Content
+    interact(chan, ['\x18', None])
+    interact(chan, ['s\r\n', None, '\r\n', None])  # Save article
+
+    # Leave Ptt
+    interact(chan, ['\x1b[D', None, '\x1b[D', None, '\r\n', None, 'y\r\n', None, '\r\n', None])
+
+
+if __name__ == '__main__':
+    main()
